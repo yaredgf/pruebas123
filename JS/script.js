742d15b9b44df4ba26363982b5c7cda512c28392
yaredgf <yaredgf@gmail.com>
2023-10-17 22:17:20 -0600

    preDefensa

--- JS/script.js
+++ JS/script.js
@@ -1,18 +1,90 @@
 
-const urlPag = "../IMG/Paginas/Pag"
-const cantPag = 10;
+const urlPag = "../IMG/Paginas/PagArtboard"
+const cantPag = 12;
+
+let barraTiempo = document.getElementById('barraTiempo'); //input range
+let audioContainer = document.getElementById('audio-container'); //contenedor
+let audio = document.getElementById('audio'); // <audio>
+let playBtn = document.getElementById('playBtn'); // playBtn
 
 function CambiarImg(num)
 {
     const contenedor = document.getElementById('contenedorPag');
     //contenedor.getElementsByClassName('img-fluid')[0].attributes.src.value;
 
-    contenedor.getElementsByClassName('img-fluid')[0].attributes.src.value= urlPag+num+'.png';
+    contenedor.getElementsByClassName('img-fluid')[0].attributes.src.value= urlPag+num+'.webp';
 }
 
 
 
+function CambiarAudioTimeStamps(pag)
+{
+    console.log(pag)
+    let nuevoTiempo= 0;
+    switch(pag)
+    {
+        case 2:
+        case 3:
+        case 4:
+            audio.pause();
+            playBtn.src="../IMG/Recursos/IconPlay.svg"
+            break;  
+        case 5:
+            nuevoTiempo= 3;
+            break;  
+            case 6:
+                nuevoTiempo= 28;
+            break; 
+            case 7:
+                nuevoTiempo= 55;
+            break; 
+            case 8:
+                nuevoTiempo= 88;
+            break; 
+            case 9:
+                nuevoTiempo= 110;
+            break; 
+            case 10:
+                nuevoTiempo= 136;
+            break; 
+            case 11:
+                nuevoTiempo= 175;
+            break; 
+            case 12:
+                audio.pause();
+                playBtn.src="../IMG/Recursos/IconPlay.svg"
+                nuevoTiempo= 175;
+            break; 
+            case 13:
+                nuevoTiempo= 190;
+            break; 
+            case 14:
 
+            break; 
+            case 15:
+
+            break; 
+            case 16:
+
+            break; 
+            case 17:
+
+            break; 
+            case 18:
+
+            break; 
+            case 19:
+
+            break; 
+            case 20:
+
+            break; 
+    }
+    
+    audio.currentTime= nuevoTiempo;
+    barraTiempo.value= nuevoTiempo;
+    
+}
 
 function CambiarContador(num)
 {
@@ -21,7 +93,8 @@ function CambiarContador(num)
     {
         let contador =parseInt(contadorPagina.value)+ parseInt(num);
         contadorPagina.value = contador;
-        CambiarImg(contador)
+        CambiarImg(contador);
+        CambiarAudioTimeStamps(contador);
     }
     else if (num==0)
     {
@@ -35,10 +108,7 @@ function CambiarContador(num)
 ////////////////////////////////////////////////////////////////////////////////////////
 
 
-let barraTiempo = document.getElementById('barraTiempo'); //input range
-let audioContainer = document.getElementById('audio-container'); //contenedor
-let audio = document.getElementById('audio'); // <audio>
-let playBtn = document.getElementById('playBtn'); // playBtn
+
 
 
 
@@ -60,7 +130,7 @@ function PlayAudio()
 
 function CambiarAudio()
 {
-    audio.currentTime=barraTiempo.value
+    audio.currentTime=barraTiempo.value;
 }
 
 function InicializarAudio()
@@ -99,13 +169,13 @@ let checkAudio = setInterval(function() {
 
 addEventListener("load", () => {
     InicializarAudio();
+    document.getElementById("contadorPagina").addEventListener("input", (event) => CambiarContador(0));
+    document.getElementById("barraTiempo").addEventListener("input", (event) => CambiarContador(0));
 
 
 
 
 });
 
-document.getElementById("contadorPagina").addEventListener("input", (event) => CambiarContador(0));
-document.getElementById("barraTiempo").addEventListener("input", (event) => CambiarContador(0));
 
 
